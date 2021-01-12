366429728212228a1573045b4163f60e57575254
Your Name <garettm123@gmail.com>
2021-01-11 21:50:42 -0600

    Removed extra bash file

--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo Compiling!
-#
-#Compile main sim
-g++ -std=c++17 -Wall -Wextra -pedantic-errors -g -o mainSim.o sim/main.cpp sim/organism.h sim/organism.cpp #
-#Compile print function#
