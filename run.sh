167cdda27cccdd0074a94c03a443add363d91136
Your Name <garettm123@gmail.com>
2021-01-11 21:47:17 -0600

    Improved file handling

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ echo Compiling!
 #Compile main sim
 g++ -std=c++17 -Wall -Wextra -pedantic-errors -g -o mainSim.o sim/main.cpp sim/organism.h sim/organism.cpp #
 #Compile print function
+#
+mkdir -p data #fsadfa
+#
 ./mainSim.o #
 echo Processing! #
 python3 py/heightPlot.py #
